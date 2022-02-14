d9146bde157299c6e7bdc90022090444121f1fdc
maingo <ngothingocmaiclht@gmail.com>
2022-02-14 16:04:55 -0500

    Add feature to enable staff to add new Category to the menu

--- app/src/main/java/com/example/foodorderserver/Home.java
+++ app/src/main/java/com/example/foodorderserver/Home.java
@@ -1,14 +1,36 @@
 package com.example.foodorderserver;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.foodorderserver.Common.Common;
+import com.example.foodorderserver.Interface.ItemClickListener;
+import com.example.foodorderserver.Model.Category;
+import com.example.foodorderserver.ViewHolder.MenuViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -16,8 +38,22 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.foodorderserver.databinding.ActivityHomeBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
 
 public class Home extends AppCompatActivity {
 
@@ -26,6 +62,39 @@ public class Home extends AppCompatActivity {
 
     private TextView txtFullName;
 
+    FirebaseDatabase database;
+    DatabaseReference categories;
+
+    FirebaseStorage storage;
+    StorageReference storageReference;
+
+    FirebaseRecyclerAdapter<Category, MenuViewHolder> adapter;
+
+    // View
+    RecyclerView recycler_menu;
+    RecyclerView.LayoutManager layoutManager;
+
+    // Add New Menu Layout
+    MaterialEditText edtName;
+    Button btnUpload, btnSelect;
+    
+    Category newCategory;
+
+    Uri savedUri;
+    private final int PICK_IMAGE_REQUEST = 71;
+
+    DrawerLayout drawer;
+
+    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result != null && result.getResultCode() == RESULT_OK
+                    && result.getData() != null && result.getData().getData() != null) {
+                savedUri = result.getData().getData();
+                btnSelect.setText("Image Selected");
+            }
+        }
+    });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,8 +109,18 @@ public class Home extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
+        // Init Firebase
+        database = FirebaseDatabase.getInstance();
+        categories = database.getReference("Category");
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener((view) -> {
+           showDialog();
+        });
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 
 //        navigationView.setNavigationItemSelectedListener(this);
@@ -52,6 +131,14 @@ public class Home extends AppCompatActivity {
         txtFullName = (TextView) headerView.findViewById(R.id.txtFullName);
         txtFullName.setText(Common.currentUser.getName());
 
+        // Init View
+        recycler_menu = (RecyclerView) findViewById(R.id.recycler_menu);
+        recycler_menu.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recycler_menu.setLayoutManager(layoutManager);
+
+        loadMenu();
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -60,6 +147,145 @@ public class Home extends AppCompatActivity {
                 .build();
     }
 
+    private void showDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(Home.this);
+        alertDialog.setTitle("Add new Category");
+        alertDialog.setMessage("Please fill in the Category's name");
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View add_menu_layout = inflater.inflate(R.layout.add_new_menu_layout, null);
+
+        edtName = add_menu_layout.findViewById(R.id.edtName);
+        btnSelect = add_menu_layout.findViewById(R.id.btnSelect);
+        btnUpload = add_menu_layout.findViewById(R.id.btnUpload);
+
+        // Event for button
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                chooseImage(); // let user select image from gallery and save URI of this image
+            }
+
+
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                uploadImage();
+            }
+        });
+        alertDialog.setView(add_menu_layout);
+        alertDialog.setIcon(R.drawable.ic_baseline_shopping_cart_24);
+
+        // Set Button
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+                // Here we create new Category
+                if (newCategory != null) {
+                    categories.push().setValue(newCategory);
+                    Snackbar.make(drawer, "New Category " + newCategory.getName() + " was added", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+            }
+        });
+        alertDialog.show();
+    }
+
+    private void uploadImage() {
+        if (savedUri != null) {
+            ProgressDialog mDialog = new ProgressDialog(this);
+            mDialog.setMessage("Uploading...");
+            mDialog.show();
+
+            String imageName = UUID.randomUUID().toString();
+            StorageReference imageFolder = storageReference.child("images/"+imageName);
+            imageFolder.putFile(savedUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            mDialog.dismiss();
+                            Toast.makeText(Home.this, "Uploaded!", Toast.LENGTH_SHORT).show();
+                            imageFolder.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    // set value for new Category if image uploaded and we could get download link
+                                    newCategory = new Category(edtName.getText().toString(), uri.toString());
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            mDialog.dismiss();
+                            Toast.makeText(Home.this, "" + e.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                            mDialog.setMessage("Uploaded " + progress + "%");
+                        }
+                    });
+
+        }
+    }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        intent = Intent.createChooser(intent, "Select Picture");
+        startForResult.launch(intent);
+    }
+    private void loadMenu() {
+        FirebaseRecyclerOptions<Category> options = new FirebaseRecyclerOptions.Builder<Category>().setQuery(categories, Category.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(options) {
+            @NonNull
+            @Override
+            public MenuViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.menu_item, parent, false);
+                return new MenuViewHolder(view);
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull MenuViewHolder holder, int position, @NonNull Category model) {
+                holder.txtMenuName.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage()).fit().into(holder.imageView);
+//                final Category clickItem = model;
+//                holder.setItemClickListener(new ItemClickListener() {
+//                    @Override
+//                    public void onClick(View view, int position, boolean isLongClick) {
+//                        // Get Category ID and send to a new activity
+//                        Intent foodList = new Intent(Home.this, FoodList.class);
+//                        // Because Category ID is key, we just get key of this item
+//                        foodList.putExtra("CategoryID", adapter.getRef(position).getKey());
+//                        startActivity(foodList);
+//                    }
+//                });
+            }
+        };
+        adapter.notifyDataSetChanged(); // Referesh data if have data changed
+        adapter.startListening();
+        recycler_menu.setAdapter(adapter);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/example/foodorderserver/Interface/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.foodorderserver.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
